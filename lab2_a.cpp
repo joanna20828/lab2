4c665b325303a136acad5109e8074bb55e5a1fc1
joanna20828 <joanna20828@gmail.com>
2015-03-31 01:22:21 +0800

    v2

--- lab2_a.cpp
+++ lab2_a.cpp
@@ -2,9 +2,9 @@
 #include<string>
 #include<fstream>
 #include<cstdlib>
-
+#include"BMI.h"
 using namespace std;
-string Category(float BMI );
+
 
 int main()
 {
@@ -28,25 +28,9 @@ int main()
 	outFile << "BMI\tCategory\n" ;
 	
 	while(inFile >> height >> weight){
-	BMI = weight/((height*0.01)*(height*0.01)) ;
-	Cat=Category(BMI); 
+	BMI=BMI.calculate(height, weight) ;
+	Cat=BMI.Category(BMI); 
 	outFile << BMI << "\t" << Cat << endl;
 	}	
 }
-string Category(float BMI)
-{
-	string Cat;
-	if(BMI<15) Cat="Very severely underweight";
-	else if(15<=BMI && BMI<16) Cat="Severely underweight";
-	else if(16<=BMI && BMI<18.5) Cat="Underweigh";
-	else if(18.5<=BMI && BMI<25) Cat="Normal";
-	else if(25<=BMI && BMI<30) Cat="Overweight ";
-	else if(30<=BMI && BMI<35) Cat="Obese Class I (Moderately obese)";
-	else if(35<=BMI && BMI<40) Cat="Obese Class II (Severely obese)";
-	else if(BMI>=40)Cat="Obese Class III (Very severely obese)";
-	return Cat;	
-}
-
-
-
 
